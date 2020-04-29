0332302712916c20ad6e11e638f63cc2ca3f07d3
Nghia Tran <vinhnghiatran@gmail.com>
2020-04-29 14:02:35 +0700

    Init Project

---
+++ example/CodeMirror-Swift-Example/CodeMirror-Swift-Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodeMirror-Swift-Example
+//
+//  Created by Nghia Tran on 4/29/20.
+//  Copyright © 2020 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ example/CodeMirror-Swift-Example/CodeMirror-Swift-Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeMirror-Swift-Example
+//
+//  Created by Nghia Tran on 4/29/20.
+//  Copyright © 2020 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
